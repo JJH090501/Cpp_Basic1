dcf0fac694fd981c02c8d5ca4a62f25b55e4e936
BSDC <seo17381738@dgsw.hs.kr>
2026-02-14 13:37:24 +0900

    first real commit

---
+++ src/conditional_statement/If.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    if (int x = 10; x > 5) {
+        cout << "x is greater than 5" << endl;
+    } else {
+        cout << "x is not greater than 5" << endl;
+    }   
+}
