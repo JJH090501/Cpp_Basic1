e8abe9c45c2a2e7d98a9eceee570fc533b230a35
BSDC <seo17381738@dgsw.hs.kr>
2026-02-14 13:44:38 +0900

    git

---
+++ src/for/for.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++) { // 변수 선언 및 조건식, 증감식 가능 C와 다름
+        cout << "Iteration " << i << endl;
+    }
+    return 0;
+}
